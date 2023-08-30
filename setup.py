2255b0d69b9d1f89a506db8d8c3c9f35d5ccddf3
Sérgio Valentim <sergioapvalentims@gmail..com>
2023-08-30 09:13:15 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
+setup(
+    name='moving_average_cli',
+    version='1.0',
+    install_requires=[
+        'pytest==7.1.1'
+    ],
     entry_points = {
         'console_scripts': [
             'moving_average = moving_average:main',
